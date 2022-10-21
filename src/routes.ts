d90670508b86c08985754689c0212cdc642e55db
kaikySantos <info.kaiky@gmail.com>
2022-10-21 12:19:37 -0300

    feat: find all deliveries available

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthenticateClientController } from "./modules/account/AuthenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/AuthenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDeliveryController/CreateDeliveryController";
+import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -15,6 +16,7 @@ const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
@@ -26,6 +28,10 @@ routes.post(
   "/delivery/",
   ensureAuthenticateClient,
   createDeliveryController.handle
+);
+routes.get(
+  "/delivery/",
+  findAllAvailableController.handle
 );
 
 export { routes };
