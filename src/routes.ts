f0ab6ae180e5bd3a26667cec11696a244f3719cf
kaikySantos <info.kaiky@gmail.com>
2022-10-21 11:54:38 -0300

    feat: create delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/AuthenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/AuthenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -12,10 +13,14 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 
+const createDeliveryController = new CreateDeliveryController();
+
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
 
 routes.post("/client/", createClientController.handle);
 routes.post("/deliveryman/", createDeliverymanController.handle);
+
+routes.post("/delivery/", createDeliveryController.handle);
 
 export { routes };
